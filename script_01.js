b741bbe82e263d041755be5fc1e0dcbd9fbfd3ed
Cing73 <craig@vancleaf>
2022-07-08 09:15:07 +0200

    First and Last Name

--- script_01.js
+++ script_01.js
@@ -10,6 +10,15 @@
 
 // Konsolen-Ausgaben
 // console.log("Check 123");
-// console.log(FirstName);
+// console.log(firstName);
 
 /*****02 Deklaration + Wertzuweisung I *******/
+
+let firstName; // Deklaration (Definition)
+firstName = "Max"; // Wertzuweisung
+console.log(firstName); // Ausgabe
+
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+console.log(familyName); // Ausgabe
+
+console.log(firstName + " " + familyName); // Deklaration + Wertzuweisung
